853a7b75f19a5c5fe01804ab42cb1eabfdf3ada0
kenzo6 <yanninckaltay@gmail.com>
2023-06-28 00:23:03 -1100

    init commit

---
+++ script.js
@@ -0,0 +1,8 @@
+alert("Welcome to world investment center");
+let myName = prompt("what your name");
+alert(`${myName} is my Name`);
+var myInvestment = prompt("how much do you want to invest");
+let Number = (`${myInvestment} / 2 `) 
+alert(`${Number}`)
+
+
